572d251d63ebff543e9952ebcdc8d3b1f1d2dfa6
pekoe09 <juha.kangas@welho.com>
2021-03-06 23:28:48 +0200

    Added some navigation items in the mainnavbar.

--- client/src/components/layout/MainHeader.js
+++ client/src/components/layout/MainHeader.js
@@ -2,28 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import MainTitle from './MainTitle'
 import MainNavBar from './MainNavBar'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const containerStyle = {
-  minHeight: 100,
+  minHeight: 60,
   minWidth: '100%',
   backgroundColor: 'lightGreen',
-}
-
-const mainHeaderStyle = {
-  margin: 10
-
 }
 
 const MainHeader = ({ titleText }) => {
   return (
     <Container fluid style={containerStyle}>
-      <Row></Row>
       <Row>
-        <div style={mainHeaderStyle}>
-          <MainTitle text={titleText} />
-          <MainNavBar />
-        </div>
+          <Col sm={4}>
+            <MainTitle text={titleText} />
+          </Col>
+          <Col>
+            <MainNavBar />
+          </Col>
       </Row>
     </Container>
   )
--- client/src/components/layout/MainNavBar.js
+++ client/src/components/layout/MainNavBar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
-import { Navbar, Nav, NavItem, Image } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, NavDropdown, Image } from 'react-bootstrap'
 import { Login, Logout } from '../users'
 
 const AnonymousItems = () => {
   return (
-    <Nav>
+    <Nav
+      style={{ width: "100%" }}
+      className='justify-content-end'
+    >
       <NavItem>
         <Login />
       </NavItem>
@@ -17,7 +20,32 @@ const AnonymousItems = () => {
 const LoggedInItems = () => {
   return (
     <React.Fragment>
-      <Nav>
+      <Nav
+        style={{ width: "100%" }}
+        className='justify-content-end'
+      >
+        <Nav.Link href='/recipes'>Recipes</Nav.Link>
+        <NavDropdown title='Beverages' id='beverages-dropdown'>          
+          <NavDropdown.Item href='/wines'>Wines</NavDropdown.Item>
+          <NavDropdown.Item href='/grapes'>Grapes</NavDropdown.Item>  
+          <NavDropdown.Item href='/winetypes'>Wine types</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href='/beers'>Beers</NavDropdown.Item>
+          <NavDropdown.Item href='/beertypes'>Beer types</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title='Static info' id='static-info-dropdown'>
+          <NavDropdown.Item href='/countries'>Countries</NavDropdown.Item>
+          <NavDropdown.Item href='/regions'>Regions</NavDropdown.Item>
+          <NavDropdown.Item href='/cuisines'>Cuisines</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href='/courses'>Courses</NavDropdown.Item>
+          <NavDropdown.Item href='/dishtypes'>Dish types</NavDropdown.Item>
+          <NavDropdown.Item href='/foodstuffs'>Foodstuffs</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href='/measuretypes'>Measure types</NavDropdown.Item>
+          <NavDropdown.Item href='/measures'>Measures</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href='/shoppinglist'>Shopping list</Nav.Link>
         <Logout />
       </Nav>
     </React.Fragment>
