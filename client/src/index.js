d43ed7a92e778285c14f9b764ea3cb5a256be7e3
Juha <juha.kangas@welho.com>
2021-03-23 17:28:35 +0200

    Reconfigured store to use redux-tools; removed react-persist

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,16 @@ import './index.css';
 import { App } from './components';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from './store'
+import store from './store'
 
 const render = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Router>
-          <React.StrictMode>
-            <App />
-          </React.StrictMode>
-        </Router>
-      </PersistGate>
+      <Router>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Router>
     </Provider>,
     document.getElementById('root')
   )
--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import { configureStore } from '@reduxjs/toolkit'
 
 import {
   beerReducer,
@@ -21,44 +17,23 @@ import {
   wineTypeReducer
 } from './reducers'
 
-const appReducer = combineReducers({
-  beers: beerReducer,
-  beerTypes: beerTypeReducer,
-  countries: countryReducer,
-  courses: courseReducer,
-  cuisines: cuisineReducer,
-  dishTypes: dishTypeReducer,
-  foodstuffs: foodstuffReducer,
-  grapes: grapeReducer,
-  ingredients: ingredientReducer,
-  measures: measureReducer,
-  measureTypes: measureTypeReducer,
-  regions: regionReducer,
-  users: userReducer,
-  wineTypes: wineTypeReducer
+const store = configureStore({
+  reducer: {
+    beers: beerReducer,
+    beerTypes: beerTypeReducer,
+    countries: countryReducer,
+    courses: courseReducer,
+    cuisines: cuisineReducer,
+    dishTypes: dishTypeReducer,
+    foodstuffs: foodstuffReducer,
+    grapes: grapeReducer,
+    ingredients: ingredientReducer,
+    measures: measureReducer,
+    measureTypes: measureTypeReducer,
+    regions: regionReducer,
+    users: userReducer,
+    wineTypes: wineTypeReducer
+  }
 })
 
-export const rootReducer = (state, action) => {
-  if (action.type === 'LOGOUT_SUCCESS') {
-    Object.keys(state).forEach(key => {
-      storage.removeItem(`persist:${key}`)
-    })
-    state = undefined
-  }
-  return appReducer(state, action)
-}
-
-export const persistConfig = {
-  key: 'limonello',
-  storage,
-  stateReconciler: autoMergeLevel2
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-export const store = createStore(
-  persistedReducer,
-  applyMiddleware(thunk)
-)
-
-export const persistor = persistStore(store)
+export default store
